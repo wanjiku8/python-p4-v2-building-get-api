f125b3c5b490a9760b8400e6c00a11a2131b5a2c
wanjiku8 <wanjikuf141@gmail.com>
2025-04-03 06:07:00 -0700

    commit

--- server/app.py
+++ server/app.py
@@ -20,6 +20,27 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
+    return make_response(games, 200)
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if game:
+        return make_response(game.to_dict(), 200)
+    else:
+        return make_response({"error": "Game not found"}, 404)
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    if game:
+        users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+        return make_response(users, 200)
+    else:
+        return make_response({"error": "Game not found"}, 404)
 
 
 if __name__ == '__main__':
